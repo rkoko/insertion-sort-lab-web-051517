1fb5d101288f5153b28166448e80d1e64fdd9fa5
rkoko <rinda.ko@gmail.com>
2017-09-10 14:51:16 -0400

    insertion sort

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 function findMinAndRemove(array){
+  let smallest = array[0]
+  let rmvSmallestIdx = 0
 
+  for (let i = 0; i<array.length; i++){
+    if(array[i]<smallest){
+      smallest = array[i]
+      rmvSmallestIdx = i
+    }
+  }
+  array.splice(rmvSmallestIdx, 1)
+  return smallest
 }
 
 function insertionSort(array){
-
+  let sortedArray = []
+  while(array.length > 0){
+    sortedArray.push(findMinAndRemove(array))
+  }
+  return sortedArray
 }

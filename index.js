f9a9d20053927ad517eef1d57d470c86f1cd4310
rkoko <rinda.ko@gmail.com>
2017-09-17 22:01:39 -0400

    insertion sort

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+//in selection sort - inner loops is over teh unsorted elements
+//in insertion sort, each pass of the inner loop iterates over the sorted elements
+
 function findMinAndRemove(array){
   let smallest = array[0]
-  let rmvSmallestIdx = 0
+  let index = 0
 
-  for (let i = 0; i<array.length; i++){
-    if(array[i]<smallest){
+  for(let i=0; i<array.length; i++){
+    if(array[i] < smallest){
       smallest = array[i]
-      rmvSmallestIdx = i
+      index = i
     }
   }
-  array.splice(rmvSmallestIdx, 1)
+  array.splice(index, 1)
   return smallest
 }
 
+
 function insertionSort(array){
   let sortedArray = []
-  while(array.length > 0){
+  while (array.length > 0){
     sortedArray.push(findMinAndRemove(array))
   }
   return sortedArray
